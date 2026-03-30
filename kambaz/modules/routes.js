c65dedc5789fb8bc365e8bdf7f2f47a47e18d580
timothybernardo <bernardo.t@northeastern.edu>
2026-03-30 14:27:55 -0400

    update modules routes

--- kambaz/modules/routes.js
+++ kambaz/modules/routes.js
@@ -1,7 +1,7 @@
 import ModulesDao from "./dao.js";
 
 export default function ModulesRoutes(app, db) {
-  const dao = ModulesDao();
+  const dao = ModulesDao(db);
 
   const findModulesForCourse = async (req, res) => {
     const { courseId } = req.params;
@@ -11,26 +11,26 @@ export default function ModulesRoutes(app, db) {
 
   const createModuleForCourse = async (req, res) => {
     const { courseId } = req.params;
-    const module = { ...req.body, course: courseId };
-    const newModule = await dao.createModule(module);
+    const module = { ...req.body };
+    const newModule = await dao.createModule(courseId, module);
     res.json(newModule);
   };
 
   const deleteModule = async (req, res) => {
-    const { moduleId } = req.params;
-    const status = await dao.deleteModule(moduleId);
+    const { courseId, moduleId } = req.params;
+    const status = await dao.deleteModule(courseId, moduleId);
     res.json(status);
   };
 
   const updateModule = async (req, res) => {
-    const { moduleId } = req.params;
+    const { courseId, moduleId } = req.params;
     const moduleUpdates = req.body;
-    const status = await dao.updateModule(moduleId, moduleUpdates);
+    const status = await dao.updateModule(courseId, moduleId, moduleUpdates);
     res.json(status);
   };
 
   app.get("/api/courses/:courseId/modules", findModulesForCourse);
   app.post("/api/courses/:courseId/modules", createModuleForCourse);
-  app.delete("/api/modules/:moduleId", deleteModule);
-  app.put("/api/modules/:moduleId", updateModule);
+  app.delete("/api/courses/:courseId/modules/:moduleId", deleteModule);
+  app.put("/api/courses/:courseId/modules/:moduleId", updateModule);
 }
