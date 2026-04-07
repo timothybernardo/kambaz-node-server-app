ef7b50978e4b82a6751effd8980f71002f05ed22
timothybernardo <bernardo.t@northeastern.edu>
2026-04-07 11:13:47 -0400

    remove domain

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    // domain: process.env.SERVER_URL,
   };
 }
 app.use(session(sessionOptions));
