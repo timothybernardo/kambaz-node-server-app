42968dbfef9da8e447b444604c79735a6f0f361b
timothybernardo <bernardo.t@northeastern.edu>
2026-04-18 16:30:42 -0400

    port a6 DAO null filter to quizzes

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -7,9 +7,11 @@ export default function EnrollmentsDao(db) {
 }
 
   async function findUsersForCourse(courseId) {
-    const enrollments = await model.find({ course: courseId }).populate("user");
-    return enrollments.map((enrollment) => enrollment.user);
-  }
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments
+    .map((enrollment) => enrollment.user)
+    .filter((user) => user !== null);
+}
 
   function enrollUserInCourse(userId, courseId) {
     return model.create({
